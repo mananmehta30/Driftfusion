e65c62b2672dbdedc612848d3f189879db3f360f
mananmehta30 <m.mehta21@imperial.ac.uk>
2022-04-07 02:05:23 +0100

    add memristor script

---
+++ Scripts/Memristor.m
@@ -0,0 +1,15 @@
+
+par_memristor = pc('Input_files/memristor');
+
+soleq_memristor = equilibrate(par_memristor);
+
+ tpoints=(2*(Vmax-Vmin)/10*k_scan)+1;
+ k_scan=0.1;
+ Vmax=5;
+ Vmin=-5;
+ 
+ 
+ % sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
+sol_CV = doCV(soleq_memristor.ion, 0, 0, Vmax, Vmin, k_scan, 10, tpoints);
+
+
