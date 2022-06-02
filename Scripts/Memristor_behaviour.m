cda9fa4bd1c078f993e38387af539b0da75e00f5
mananmehta30 <m.mehta21@imperial.ac.uk>
2022-06-02 18:18:17 +0100

    reset to original values

--- Scripts/Memristor_behaviour.m
+++ Scripts/Memristor_behaviour.m
@@ -12,9 +12,10 @@ soleq_memristor_interface_reactions = equilibrate(par_memristor_interface_reacti
 
 %% Cyclic Voltamogram scan
 % sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
- k_scan = 0.001;
-Vmax = 5;
- Vmin = -5;
+ k_scan = 0.1;
+Vmax = 1;
+ Vmin = -1;
 tpoints=(2*(Vmax-Vmin)/(10*k_scan))+1;
 sol_CV = doCV(soleq_memristor.ion, 0, 0, Vmax, Vmin, k_scan, 1, tpoints);
 dfplot.ELnpx(sol_CV, 0);
+
