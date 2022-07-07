5dc4446e4d07de40ee40bdd600c77675da7a62ee
mananmehta30 <m.mehta21@imperial.ac.uk>
2022-07-07 18:32:04 +0100

    updated dfana for the j.c and j.a terms

--- Core/dfana.m
+++ Core/dfana.m
@@ -215,8 +215,10 @@ classdef dfana
                 j.p = jp_l + deltajp;
             end
 
-            j.a = 0 + deltaja;
-            j.c = jc_l + jc_r + deltajc;
+           % j.a = 0 + deltaja;
+            %j.c = jc_l + jc_r + deltajc;
+                 j.c = jc_l + deltajc;
+                j.a = ja_l + deltaja;
 %             j.c = 0 + deltajc; % old cation flux term
             % displacement flux
             FV_ihalf = dfana.calcF_ihalf(sol);
