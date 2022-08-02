400cb4725508ede8e921ef4382fdc49e3a631a63
mananmehta30 <m.mehta21@imperial.ac.uk>
2022-08-02 15:05:58 +0100

    Update Memristor.m

--- Scripts/Memristor.m
+++ Scripts/Memristor.m
@@ -6,8 +6,10 @@ soleq_memristor = equilibrate(par_memristor);
  Vmax=5;
  Vmin=-5;
 k_scan=0.1;
- tpoints=(2*(Vmax-Vmin)/10*k_scan)+1;
 
+%%
  % sol_CV = doCV(sol_ini, light_intensity, V0, Vmax, Vmin, scan_rate, cycles, tpoints)
 
- sol_CV = doCV(soleq_memristor.el, 0, 0, Vmax, Vmin, k_scan, 10, tpoints);
+ sol_CV = doCV(soleq_memristor.el, 0, 0, Vmax, Vmin, k_scan, 10, 400);
+%%
+dfplot.JtotVapp(sol_CV,1);
